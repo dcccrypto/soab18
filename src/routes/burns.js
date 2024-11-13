5602cf30ca181708a7dff0c6a26a4a5543a6cecc
khubair <khubairnasir26@gmail.com>
2024-11-13 10:01:25 +0000

    fixing roadmap

--- src/routes/burns.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const burnController = require('../controllers/burnController');
-
-router.get('/tokens/:tokenAddress/burns', burnController.getBurnHistory);
-
-module.exports = router; 
--- src/routes/tokens.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const tokenController = require('../controllers/tokenController');
-
-router.get('/tokens/:tokenAddress/holders', tokenController.getHolderStats);
-router.get('/tokens/:tokenAddress/price', tokenController.getTokenPrice);
-
-module.exports = router; 
