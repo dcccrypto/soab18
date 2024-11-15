c5b770c0b4e083f2d3fc0d7235dd6e7dc1f1b23f
khubair <khubairnasir26@gmail.com>
2024-11-15 03:09:45 +0000

    Ready for Vercel deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
   images: {
     domains: [
       'hebbkx1anhila5yf.public.blob.vercel-storage.com'
     ],
     unoptimized: true
   },
-  reactStrictMode: true,
-  poweredByHeader: false,
-  compress: true,
-  distDir: '.next'
+  distDir: '.next',
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
+    return config;
+  }
 }
 
 module.exports = nextConfig 
