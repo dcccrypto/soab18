6fd5cf80e208e770c957a53777ad68988d5c0bb1
khubair <khubairnasir26@gmail.com>
2024-11-15 03:00:24 +0000

    Ready for Vercel deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,40 +6,7 @@ const nextConfig = {
     ],
     unoptimized: true
   },
-  reactStrictMode: true,
-  poweredByHeader: false,
-  compress: true,
-  productionBrowserSourceMaps: false,
-  swcMinify: true,
-  output: 'standalone',
-  experimental: {
-    optimizeCss: true,
-    scrollRestoration: true
-  },
-  async headers() {
-    return [
-      {
-        source: '/:all*(svg|jpg|png)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable'
-          }
-        ]
-      },
-      {
-        source: '/_next/image/:all*',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable'
-          }
-        ]
-      }
-    ]
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig 
